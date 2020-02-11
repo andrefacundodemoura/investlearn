ebd8986bfb1dc9fef3c007e513a7d4b555757a41
Andre Facundo <andrefacundodemoura@gmail.com>
2020-02-11 19:54:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function selic(){
+    window.alert('ola')
+
+}
+function tesouropre(){
+    window.alert('ola')
+
+}
+function ipca(){
+    window.alert('ola')
+
+}
+function calcular(){
+    window.alert('ola')
+    
+}
